269c578c1459ed6d7b62679246c56912b0a5c3b0
Rob Fisher <robfisher@gmail.com>
2020-09-27 17:19:27 +0000

    Script to update every day.

--- README.md
+++ README.md
@@ -4,5 +4,3 @@ Produces some charts I find interesting.
 Uses the UK Covid-19 API here: https://pypi.org/project/uk-covid19/
 
 Results publushed here: http://rfcovid.surge.sh/
-
-TODO: script to automatically update every day.
---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export PATH=~/.local/bin:$PATH # use user's Python and Jupyter installations
+export TODAY=$(date '+%Y-%m-%d')
+export CSV_FILENAME=covid_${TODAY}.csv
+
+python3.7 fetch_csv.py $CSV_FILENAME
+export FETCH_TIME=$(date)
+
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=600 --ExecutePreprocessor.kernel_name='python3' --to html --no-input analysis.ipynb
+
+mkdir -p output
+rm -f output/*.csv
+mkdir -p archive
+cp analysis.html output/index.html
+cp covid-processed-${TODAY}.csv output/
+
+cp analysis.html archive/analysis-${TODAY}.html
+cp covid-processed-${TODAY}.csv archive/
+cp ${CSV_FILENAME} archive/
+
+cd output
+surge --domain http://rfcovid.surge.sh $PWD
